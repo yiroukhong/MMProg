faa6905968370356c4b2255c13288fc5b993aa71
Yirou <khongyirou2005@gmail.com>
2026-04-28 12:22:47 +0800

    Add FavouritesManager for managing favorite images and verified functionality with tests

--- WIG3003-PhotoApp/src/test/java/com/wig3003/photoapp/AppTest.java
+++ WIG3003-PhotoApp/src/test/java/com/wig3003/photoapp/AppTest.java
@@ -1,20 +1,13 @@
 package com.wig3003.photoapp;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
     }
 }
